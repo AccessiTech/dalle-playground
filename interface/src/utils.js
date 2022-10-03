77658c395c06a7ea62c1b28203ffdb276e47a8ce
AccessiT3ch <accessiT3ch@gmail.com>
2022-10-02 17:17:31 -0700

    Add utility function to convert seconds to hh:mm:ss

--- interface/src/utils.js
+++ interface/src/utils.js
@@ -8,3 +8,24 @@ export function isValidURL(str) {
     '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
   return !!pattern.test(str);
 }
+
+/**
+ * Generate Time String
+ * @param {number|string} time the time in seconds
+ * @returns {string} the time in a human readable format
+ * @example 600 -> 10:00
+ */
+export function generateTimeString(time) {
+  if (time < 60) return "00:00:" + (time < 10 ? "0" + time : time);
+
+  let hours = Math.floor(time / 3600);
+  let minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time % 60);
+
+  if (hours > 0) minutes = minutes % 60;
+  if (hours < 10) hours = "0" + hours;
+  if (minutes < 10) minutes = "0" + minutes;
+  if (seconds < 10) seconds = "0" + seconds;
+
+  return hours + ":" + minutes + ":" + seconds;
+}
